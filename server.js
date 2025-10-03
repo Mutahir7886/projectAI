3d246c18253368c5908ed984cf42bed16dd92567
Mutahir7886 <68272346+Mutahir7886@users.noreply.github.com>
2025-10-03 19:59:33 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
-
-const router = require("./routes/dataRoutes"); // if you’re using routes.js
-// OR: const { askStock } = require("./controllers/dataController");
-
+const toolRoutes = require("./routes/toolRoutes");
+require("dotenv").config();
 
 const app = express();
+
 app.use(express.json());
+// app.use("/api", router); // data based 
+app.use("/api", toolRoutes);
 
-// Using routes.js
-app.use("/api", router);
-
-// OR without routes.js:
-// app.post("/api/ask", askStock);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
