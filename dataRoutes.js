2f5dad6764e3fe83f3e33d0c8798d953118a75ab
Mutahir7886 <68272346+Mutahir7886@users.noreply.github.com>
2025-10-03 19:57:03 +0500

    Delete dataRoutes.js

--- dataRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { askStock } = require("../controllers/dataController");
-
-router.post("/ask", askStock);
-
-module.exports = router;
